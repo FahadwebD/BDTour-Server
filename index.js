302499fd6876b5d4170016aae330bfd356215732
FahadwebD <fahadchowdhury66779@gmail.com>
2021-10-29 02:43:42 +0600

    add data onclick

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ffrgt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run (){
+    try{
+        await client.connect();
+        const database = client.db('bdTour');
+        const toursCollection = database.collection('tours');
+        const booking_Collection = database.collection("books");
+
+
+        //Get Tours List
+
+        app.get('/tours' , async(req , res)=>{
+            const cursor = toursCollection.find({});
+            const tours = await cursor.toArray();
+            res.send(tours);
+        })
+
+        //Add Data To Database
+
+        app.post("/books/add", async (req, res) => {
+      const tour = req.body;
+      const result = await booking_Collection.insertOne(tour);
+      res.json(result);
+    });
+
+    //get booking data 
+    app.get('/books' , async(req , res)=>{
+        const cursor = booking_Collection.find({});
+        const books = await cursor.toArray();
+        res.send(books)
+    })
+
+
+
+    }
+    finally{
+        // await client.close();
+    }
+
+}
+
+run().catch(console.dir);
 
 app.get('/' , (req , res) =>{
     res.send('server running');
